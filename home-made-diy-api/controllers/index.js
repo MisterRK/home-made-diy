05d254dfb7d7171086f93e73c0595df7bd76f20c
MisterRK <robert.keller22@gmail.com>
2020-10-30 15:37:33 -0400

    updateUser route and controlled tested.

--- home-made-diy-api/controllers/index.js
+++ home-made-diy-api/controllers/index.js
@@ -46,8 +46,26 @@ const getUserById = async (req, res) => {
    }
 }
 
+const updateUser = async (req, res) => {
+   try {
+      const { id } = req.params;
+      const [ updated ] = await User.update(req.body, {
+         where: { id: id }
+      });
+      if(updated) {
+         const updatedUser = await User.findOne({where: {id: id}
+         });
+         return res.status(200).json({user: updatedUser});
+      }
+      return res.status(404).send('User with the specified ID does no exist')
+   } catch (error) {
+      return res.status(500).send(error.message);
+   }
+};
+
 module.exports = {
    createUser,
    getAllUsers,
-   getUserById
+   getUserById,
+   updateUser
 }
--- home-made-diy-api/routes/index.js
+++ home-made-diy-api/routes/index.js
@@ -8,5 +8,6 @@ router.get('/', (req, res) => res.send('This is root!'))
 router.post('/users', controllers.createUser)
 router.get('/users', controllers.getAllUsers)
 router.get('/users/:id', controllers.getUserById)
+router.put('/users/:id', controllers.updateUser)
 
 module.exports = router
