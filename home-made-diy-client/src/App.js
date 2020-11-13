2cc36ba07560e4b3101d38de436dc4da75b8c366
MisterRK <robert.keller22@gmail.com>
2020-11-12 23:48:56 -0500

    Added skeleton for projects container.

--- home-made-diy-client/src/App.js
+++ home-made-diy-client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 //component imports
 import Header from './components/Header/Header'
+import ProjectsContainer from './components/ProjectsContainer/ProjectsContainer';
 
 class App extends React.Component {
   render(){
     return (
       <div className="App">
         <Header />
+        <ProjectsContainer/>
       </div>
     );
   }
---
+++ home-made-diy-client/src/components/ProjectsContainer/ProjectsContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//bootstrap imports
+import Container from 'react-bootstrap/Container'
+
+const ProjectsContainer = () => {
+   return (
+      <Container>
+         <h1>Let's Look at Some projects</h1>
+      </Container>
+   );
+};
+
+export default ProjectsContainer;
