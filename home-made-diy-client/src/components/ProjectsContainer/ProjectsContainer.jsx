b694bafcedf1e6bae1f74d40801e7646af374c35
MisterRK <robert.keller22@gmail.com>
2020-11-19 12:42:05 -0500

    Added spinner for projects loading

--- home-made-diy-client/src/components/ProjectsContainer/ProjectsContainer.jsx
+++ home-made-diy-client/src/components/ProjectsContainer/ProjectsContainer.jsx
@@ -1,30 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 //component imports
-import ProjectCard from './ProjectCard/ProjectCard'
+import ProjectCard from "./ProjectCard/ProjectCard";
 
 //bootstrap imports
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-
+import { Container, Row, Spinner } from "react-bootstrap";
 
 const ProjectsContainer = () => {
-   const [ projects, setProjects ] = useState(null)
+	const [projects, setProjects] = useState(null);
 
-   useEffect(() => {
-      axios.get('http://localhost:5000/api/projects/')
-      .then(res => setProjects(res.data.projects))
-   }, [])
+	// useEffect(() => {
+	//    axios.get('http://localhost:5000/api/projects/')
+	//    .then(res => setProjects(res.data.projects))
+	// }, [])
 
-   return (
-      <Container>
-         <h1>Let's Look at Some projects</h1>
-         <Row className="justify-content-between ">
-            {projects && projects.map(project => <ProjectCard project={project} />)}
-         </Row>
-      </Container>
-   );
+	return (
+		<Container>
+			<h1 style={{ textAlign: "center" }}>Let's Look at Some projects</h1>
+			{!projects && (
+				<div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', padding: "25%"}}>
+					<Spinner
+						style={{
+							height: "250px",
+							width: "250px",
+						}}
+						animation="border"
+					>
+						<span className="sr-only">Loading...</span>
+					</Spinner>
+				</div>
+			)}
+			<Row className="justify-content-center">
+				{projects &&
+					projects.map((project) => <ProjectCard project={project} />)}
+			</Row>
+		</Container>
+	);
 };
 
 export default ProjectsContainer;
