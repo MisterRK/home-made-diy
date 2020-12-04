8f6895085c20ef96f45830325f9d14d84cda9021
MisterRK <robert.keller22@gmail.com>
2020-12-03 23:59:44 -0500

    added form to projectCreation interface

--- home-made-diy-client/src/components/ProjectCreationInterface/ProjectCreationInterface.jsx
+++ home-made-diy-client/src/components/ProjectCreationInterface/ProjectCreationInterface.jsx
@@ -1,8 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 
+import {Form, Button} from 'react-bootstrap'
+import ProjectImagePreview from '../CreateNewProject/NewProjectForm/ProjectImagePreview/ProjectImagePreview'
+
 const ProjectCreationInterface = (props) => {
    const [project, setProject] = useState(null);
+
    const [step, setStep] = useState({
       heading: "",
       content: "",
@@ -35,9 +39,39 @@ const ProjectCreationInterface = (props) => {
 		}
 	};
    return (
-      <div>
-         
-      </div>
+      <Form encType="multipart/form-data">
+						<Form.Group controlId="formGroupStepTitle">
+							<Form.Label>Add a heading to your step:</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Tell me what we're going to do in this step"
+								value={step.heading}
+								name="heading"
+								onChange={handleChange}
+							/>
+						</Form.Group>
+						<Form.Group controlId="formGroupStepContent">
+							<Form.Label>Details for step 1: </Form.Label>
+							<Form.Control
+								as="textarea"
+								rows="10"
+								value={step.content}
+								name="content"
+								onChange={handleChange}
+							/>
+						</Form.Group>
+						<Form.Group>
+							<Form.Label>
+								Add a photo to go along with this step
+							</Form.Label>
+							<Form.File name="stepImage" onChange={handleChange} />
+							{step.stepImage !== "" ? (
+								<ProjectImagePreview image={step.stepImage} />
+							) : null}
+						</Form.Group>
+						<Button>Add Another Step</Button>
+						<Button>Publish Project</Button>
+					</Form>
    );
 };
 
